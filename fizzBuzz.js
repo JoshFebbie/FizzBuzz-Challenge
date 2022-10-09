86547f530583cedaa1987f6a32d22b5a3a4021cb
Josh Febbie <joshdfebbie@gmail.com>
2022-10-09 10:23:19 -0700

    My-FizzBuzz-Solution

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+// Write your solution below this line:
+
+function fizzbuzz(num) {
+
+    for (let i = 1; i <= num; i++) {
+        if(i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0)console.log("Buzz");
+        else if (i % 5 === 0)console.log("Fizz");
+        else console.log(i);
+      }
+    }
+    
+    fizzbuzz(30)
